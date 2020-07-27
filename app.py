0db25846c2b4209a48365b609dc8662c4b9f284e
pkluitenberg <pkluitenberg@spins.com>
2020-07-26 22:08:10 -0500

    cleaning up flask app

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-import time
 import requests
 from flask import Flask
 
 app = Flask(__name__)
-
-@app.route('/time')
-def get_current_time():
-    return {'time':time.time()}
 
 # image function
 @app.route('/image')
