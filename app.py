e608581d9286a5463dd118e2369ac627ea4a9a38
Paul Kluitenberg <paul.kluitenberg@gmail.com>
2020-07-26 22:21:40 -0500

    PK: updated comments

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# image function
+# returns url of random Unsplash image when endpoint is hit
 @app.route('/image')
 def get_random_image():
     url = 'https://source.unsplash.com/random/800x600'
