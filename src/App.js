2baa49fb766024735a437227db84e0238fa9ecb6
Paul Kluitenberg <paul.kluitenberg@gmail.com>
2021-12-06 02:56:49 -0600

    * formatting * removing html tags

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React, { useState, useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-  const [currentImage, getCurrentImage] = useState(0);
+    const [currentImage, getCurrentImage] = useState(0);
 
-  useEffect(() => {
-    fetch('/image').then(res => res.json()).then(data => {
-      getCurrentImage(data.random_image_url);
-    });
-  }, []);
+    useEffect(() => {
+        fetch('/image').then(res => res.json()).then(data => {
+            getCurrentImage(data.random_image_url);
+        });
+    }, []);
 
-  return (
-    <html>    
-      <div className="App">
-        <header className="App-header">
-          <img src={currentImage} className="App-image"/>
-        </header>
-      </div>
-    </html>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={currentImage} className="App-image" alt={"It's random. I don't know what this is gonna be"}/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
